0a69b7feaeea2f7928d5aeea2bd94d564e865882
Loris Zinsou <the.lz.himself@gmail.com>
2018-02-27 10:30:59 +0100

    Remove format_execution_result from common utils

--- graphql_env/server/common/utils.py
+++ graphql_env/server/common/utils.py
@@ -1,11 +1,8 @@
-from collections import namedtuple
 import json
 import six
 
 from ...params import GraphQLParams
 from .exceptions import InvalidVariablesJSONError
-
-GraphQLResponse = namedtuple('GraphQLResponse', 'result,status_code')
 
 
 def execution_result_to_dict(execution_result, format_error):
@@ -17,28 +14,6 @@ def execution_result_to_dict(execution_result, format_error):
     if execution_result.data and not execution_result.invalid:
         data['data'] = execution_result.data
     return data
-
-
-def format_execution_result(execution_result, format_error):
-    status_code = 200
-
-    if execution_result:
-        response = {}
-
-        if execution_result.errors:
-            response['errors'] = [format_error(e)
-                                  for e in execution_result.errors]
-
-        if execution_result.invalid:
-            status_code = 400
-        else:
-            status_code = 200
-            response['data'] = execution_result.data
-
-    else:
-        response = None
-
-    return GraphQLResponse(response, status_code)
 
 
 def graphql_params_from_data(query_params, data=None):
