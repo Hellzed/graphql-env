d6fdc06c6eb165283b457f14741b104843c7143b
Loris Zinsou <the.lz.himself@gmail.com>
2018-02-27 10:31:14 +0100

    WIP

--- graphql_env/server/aiohttp/graphql_view.py
+++ graphql_env/server/aiohttp/graphql_view.py
@@ -1,23 +1,29 @@
 from collections import MutableMapping, namedtuple
 
 from aiohttp.web import json_response, Response, View
-from graphql.error import format_error as default_format_error
+from graphql.error import GraphQLError, format_error as format_graphql_error
 from graphql.execution import ExecutionResult
 from promise import Promise
 
 from ..common.exceptions import (BatchEmptyListError, BatchNotEnabledError,
-                                 GraphQLHTTPError, InvalidJSONError,
-                                 NotADictError)
+                                 InvalidJSONError, NotADictError)
 from ..common.utils import execution_result_to_dict, graphql_params_from_data
+
+GraphQLResponse = namedtuple('GraphQLResponse', 'result,status_code')
 
 GRAPHQL_ENV = 'graphql_env'
 GET_ALLOWED_OPERATIONS = set(("query", ))
 POST_ALLOWED_OPERATIONS = set(("query", "mutation", "subscription"))
 
-GraphQLHTTPResponse = namedtuple('GraphQLHTTPResponse', 'result,status_code')
+
+def default_format_error(error):
+    if isinstance(error, GraphQLError):
+        return format_graphql_error(error)
+
+    return {'message': str(error)}
 
 
-def display_graphiql(request):
+def can_display_graphiql(request):
     if 'raw' in dict(request.query):
         return False
 
@@ -31,48 +37,82 @@ def display_graphiql(request):
 
 async def parse_body(request):
     content_type = request.content_type
+    data = {}
 
     if content_type == 'application/graphql':
-        return {'query': await request.text()}
+        text = await request.text()
+        data = {'query': text}
     elif content_type == 'application/json':
         try:
-            return await request.json()
+            data = await request.json()
         except:
             raise InvalidJSONError()
     else:
-        return dict(await request.post())
+        form = await request.post()
+        data = dict(form)
+
+    return data
 
 
-async def get_graphql_http_response(request, params, allowed_operations):
+async def get_result(request, params, allowed_operations):
     graphql_env = request.app[GRAPHQL_ENV]
     middleware = graphql_env.middleware
     execute = graphql_env
-    status = 200
 
     try:
-        execution_result = await Promise.resolve(execute(
+        return execute(
             params,
             root=None,
             context=request,
             middleware=middleware,
             allowed_operations=allowed_operations
-        ))
+        )
     except Exception as error:
-        if isinstance(error, GraphQLHTTPError):
-            status = error.status_code
-        execution_result = ExecutionResult(errors=[error])
+        return ExecutionResult(errors=[error])
 
-    return GraphQLHTTPResponse(execution_result, status)
+
+def format_one_result(result, format_error):
+    status_code = 200
+
+    if result:
+        response = execution_result_to_dict(result, format_error)
+        status_code = 400 if result.invalid else 200
+    else:
+        response = None
+
+    return GraphQLResponse(response, status_code)
+
+
+def format_many_results(results, format_error):
+    responses = [
+        format_one_result(result, format_error)
+        for result in results
+    ]
+    formatted_results, status_codes = zip(*responses)
+    status_code = max(status_codes)
+
+    return formatted_results, status_code
+
+
+def graphql_json_response(results, format_error=default_format_error,
+                          is_batch=False):
+    if is_batch:
+        response, status_code = format_many_results(results, format_error)
+    else:
+        response, status_code = format_one_result(results, format_error)
+
+    return json_response(response, status=status_code)
 
 
 class GraphQLView(View):
     async def get(self):
         allowed_operations = GET_ALLOWED_OPERATIONS
         params = graphql_params_from_data(dict(self.request.query))
-        result, status = await get_graphql_http_response(self.request, params, allowed_operations)
+        result = await Promise.resolve(
+            get_result(self.request, params, allowed_operations)
+        )
 
-        awaited_execution_result = await Promise.all([result])
-        return json_response(execution_result_to_dict(awaited_execution_result[0], default_format_error), status=status)
+        return graphql_json_response(result)
 
     async def post(self):
         batch_enabled = True
@@ -101,24 +141,14 @@ class GraphQLView(View):
         params_batch = [graphql_params_from_data(query_data, entry)
                         for entry in body_data]
 
-        responses = [get_graphql_http_response(
-                self.request,
-                params,
-                allowed_operations)
-            for params in params_batch]
-
-        awaited_execution_results = await Promise.all(responses)
-
-        results, status_codes = zip(*awaited_execution_results)
-        status = max(status_codes)
-
-        final_result = [execution_result_to_dict(result, default_format_error)
-                        for result in results]
-
+        result = await Promise.all(
+            [get_result(self.request, params, allowed_operations)
+             for params in params_batch]
+        )
         if not is_batch:
-            final_result = final_result[0]
+            result = result[0]
 
-        return json_response(final_result, status=status)
+        return graphql_json_response(result, is_batch=is_batch)
 
     async def options(self):
         """ Preflight request support
